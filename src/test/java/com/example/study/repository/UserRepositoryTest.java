5a7dbba5b3a08b58733cac1bf412fd0e19cff7c1
HyeJi <hyeji9530@gmail.com>
2021-03-26 17:49:52 +0900

    JPA를 통한 CRUD 배우기-1_2

--- src/test/java/com/example/study/repository/UserRepositoryTest.java
+++ src/test/java/com/example/study/repository/UserRepositoryTest.java
@@ -4,8 +4,10 @@ import com.example.study.StudyApplicationTests;
 import com.example.study.model.entity.User;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.time.LocalDateTime;
+import java.util.Optional;
 
 public class UserRepositoryTest extends StudyApplicationTests {
 
@@ -28,12 +30,29 @@ public class UserRepositoryTest extends StudyApplicationTests {
 
     }
 
+    @Test
     public void read(){
+        Optional<User> user = userRepository.findById(2L);
 
+        user.ifPresent(selectUser ->{
+            System.out.println("user : "+selectUser);
+            System.out.println("email : "+selectUser.getEmail());
+        });
     }
 
+    @Test
     public void update(){
 
+        // update user set account=%?
+        Optional<User> user = userRepository.findById(2L);
+
+        user.ifPresent(selectUser ->{
+           selectUser.setAccount("PPPP");
+           selectUser.setUpdatedAt(LocalDateTime.now());
+           selectUser.setUpdatedBy("update method()");
+
+           userRepository.save(selectUser);
+        });
     }
 
     public void delete(){
