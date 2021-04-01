a200c80ef3764590bc32fdae751078373c202511
HyeJi <hyeji9530@gmail.com>
2021-04-01 18:14:28 +0900

    연관관계 설정-1

--- src/main/java/com/example/study/model/entity/OrderDetail.java
+++ src/main/java/com/example/study/model/entity/OrderDetail.java
@@ -13,7 +13,7 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity // order_detail
-//@ToString(exclude = {"user","item"})
+@ToString(exclude = {"orderGroup"})
 public class OrderDetail {
 
     @Id
@@ -38,13 +38,8 @@ public class OrderDetail {
 
     private Long itemId;
 
-    private Long orderGroupId;
+    // OrderDetail N : 1 OrderGroup
+    @ManyToOne
+    private OrderGroup orderGroup;
 
-    // N : 1
-    //@ManyToOne
-    //private User user; // user_id
-
-    // N : 1
-    //@ManyToOne
-    //private Item item;
 }
