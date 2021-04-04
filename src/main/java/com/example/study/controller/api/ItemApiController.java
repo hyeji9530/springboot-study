0f50cbe2f3f76a188ff7274325bb5257706a7c2f
HyeJi <hyeji9530@gmail.com>
2021-04-04 22:48:54 +0900

    상품 API DELETE

--- src/main/java/com/example/study/controller/api/ItemApiController.java
+++ src/main/java/com/example/study/controller/api/ItemApiController.java
@@ -37,6 +37,6 @@ public class ItemApiController implements CrudInterface<ItemApiRequest, ItemApiR
     @Override
     @DeleteMapping("{id}") // /api/item/1... 1000
     public Header delete(@PathVariable Long id) {
-        return null;
+        return itemApiLogicService.delete(id);
     }
 }
