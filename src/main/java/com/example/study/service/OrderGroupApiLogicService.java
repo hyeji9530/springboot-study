b2105a9a2fb9a723d86bc1d44e3eb9ca8e98ee11
HyeJi <hyeji9530@gmail.com>
2021-04-06 20:53:47 +0900

    주문내역 API READ

--- src/main/java/com/example/study/service/OrderGroupApiLogicService.java
+++ src/main/java/com/example/study/service/OrderGroupApiLogicService.java
@@ -46,7 +46,13 @@ public class OrderGroupApiLogicService implements CrudInterface<OrderGroupApiReq
 
     @Override
     public Header<OrderGroupApiResponse> read(Long id) {
-        return null;
+        return orderGroupRepository.findById(id)
+                /*.map(orderGroup -> {
+                    return response(orderGroup);
+                })*/
+                //.map(orderGroup -> response(orderGroup))
+                .map(this::response)
+                .orElseGet(()->Header.ERROR("데이터 없음"));
     }
 
     @Override
